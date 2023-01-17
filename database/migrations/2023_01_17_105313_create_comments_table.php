79372fe2b64963df4eda921b3d4535972885b9c8
m-shokida <mitsuru.shokida@gmail.com>
2023-01-17 23:27:35 +0900

    Migrations

---
+++ database/migrations/2023_01_17_105313_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignUuid('topic_id')->constrained();
+            $table->ipAddress();
+            $table->text('comment');
+            $table->string('image_url');
+            $table->integer('plus_vote_count');
+            $table->integer('minus_vote_count');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
